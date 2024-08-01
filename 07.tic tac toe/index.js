ddd844e4268bdc56354697174206b47768f166ec
thanwin84 <thanwinhline84@gmail.com>
2024-08-01 20:55:22 +0600

    update tic tac toe project

--- 07.tic tac toe/index.js
+++ 07.tic tac toe/index.js
@@ -1,175 +1,128 @@
+const boardRef = document.getElementById('board')
+const currentStatus = document.querySelector('#currentStatus')
 
-const boardRef = document.getElementById("tic-tac-toe")
-const h2 = document.querySelector('h2')
-const button = document.querySelector('button')
-
-let currentMove = 'X'
-let game;
-let rightD = []
+let currentPlayer = 'X'
+let gameBoard;
 let gridSize = 3
 
-function initializeRightD(){
-    let row = 0, col = gridSize -1
-    while (row < gridSize && col >= 0){
-        rightD.push([row, col])
-        row += 1
-        col -= 1 
-    }
-}
-
-function createBoard(gridSize){
-    for (let i = 0; i < gridSize; i++){
+function createBoard(){
+    for (let r = 0; r < gridSize; r++){
         const row = document.createElement('div')
         row.classList.add('row')
-        for (let j = 0; j < gridSize; j++){
-           
-            const cellRef = document.createElement('div')
-            cellRef.classList.add('cell')
-            cellRef.setAttribute('data-row', i)
-            cellRef.setAttribute('data-col', j)
-            row.appendChild(cellRef)
+        for (let c = 0; c < gridSize; c++){
+            const cell = document.createElement('div')
+            cell.classList.add('cell')
+            cell.setAttribute('data-row', r)
+            cell.setAttribute('data-col', c)
+            row.appendChild(cell)
         }
         boardRef.appendChild(row)
     }
 }
 
 function addEventListener(){
-    boardRef.addEventListener('click', handleCellClick)
-}
-
-function handleCellClick(event){
-    const target = event.target
-        if (target.classList.contains('cell')){
-            const row = target.dataset.row
-            const col = target.dataset.col
-
-            if (game[row][col] === 0){
-                // set current div with current move
-                target.textContent = currentMove
-                updateGame(row, col, currentMove)
-                const {win,...rest} = getWinner(row, col)
-
-                if (win){
-                    displayWinner()
-                    highlightWinningCell(row, col, rest)
-
-                } else {
-                    changePlayer()
-                }
+    boardRef.addEventListener('click', function(event){
+        if(event.target.classList.contains('cell')){
+            const target = event.target
+            target.innerHTML = currentPlayer
+            const row = Number(target.dataset.row)
+            const col = Number(target.dataset.col)
+            gameBoard[row][col] = currentPlayer
+            const {hasWon, winningCell} = checkWinner(row, col)
+            if (hasWon){
+                currentStatus.textContent = `${currentPlayer} has won`
+                markWinningCells(winningCell, row, col)
+            }
+            else {
+                changePlayer()
             }
         }
+    })
 }
 
 function changePlayer(){
-    currentMove = currentMove === 'X' ? 'Y': 'X'
-    h2.textContent = currentMove === 'Y' ? "Player 2's turn": "player 1's turn"
-}
-
-function updateGame(row, col, value){
-    game[row][col] = value
+    currentPlayer = currentPlayer === 'X' ? 'Y': 'X'
+    currentStatus.textContent = `${currentPlayer}'s Move`
     
 }
-function displayWinner(){
-    const winner = currentMove === 'X' ? 1 : 2
-    h2.textContent = `Player ${winner} won`
+
+function fillBoard(){
+    gameBoard = Array.from(Array(gridSize), ()=> Array(gridSize).fill('0'))
 }
-function getWinner(row, col){
-    // check row
-    let current = game[row][col]
+
+function checkWinner(row, col){
     let rowWin = true
     let colWin = true
+    let leftDiagonalWin = true
+    let righDiagonalWin = true
+    // check row
     for (let i = 0; i < gridSize; i++){
-        if (game[row][i] !== current){
+        if (gameBoard[row][i] !== currentPlayer){
             rowWin = false
-            
         }
-        if (game[i][col] !== current){
+    }
+    // check col
+    for (let i = 0; i < gridSize; i++){
+        if (gameBoard[i][col] !== currentPlayer){
             colWin = false
         }
     }
+    // check left diagonal
+    // check right diagonal
+    let r = 0, lc = 0
+    let  rc = gridSize - 1
     
-    let r = 0
-    let c = gridSize - 1
-    let c2 = 0
-    let leftDiagonalWin = true
-    let rightDiagonalWin = true
-    while (r < gridSize && c >= 0 && c2 < gridSize){
-        if (game[r][c] !== current){
-            rightDiagonalWin = false
-        }
-        if (game[r][c2] !== current){
+    while (r < gridSize && lc < gridSize && rc >= 0){
+        
+        if (gameBoard[r][lc] !== currentPlayer){
             leftDiagonalWin = false
         }
-       
-        r += 1
-        c -= 1
-        c2 += 1
-    }
-    const win =  rowWin || colWin || leftDiagonalWin || rightDiagonalWin
-    
-    return {win, rowWin, colWin, leftDiagonalWin, rightDiagonalWin}
-}
-
-function resetGame(){
-    const cells = document.querySelectorAll('.cell')
-    cells.forEach(cell => {
-        cell.textContent = ""
-        if (cell.classList.contains('winning')){
-            cell.classList.remove('winning')
+        if (gameBoard[r][rc] !== currentPlayer){
+            righDiagonalWin = false
         }
-    })
-    h2.textContent = "Player 1's turn"
-    game = Array.from(Array(3), () => Array(3).fill(0))
-    currentMove = 'X'
+        r++
+        lc++
+        rc--
+
+    }
+    const hasWon = rowWin || colWin || leftDiagonalWin || righDiagonalWin
+    return {hasWon, winningCell: rowWin ? 'rowWin': colWin ? "colWin": leftDiagonalWin ? "leftDiagonalWin": righDiagonalWin ? 'righDiagonalWin': "not won"}
 
 }
 
-
-function highlightWinningCell(row, col, winningCells){
-    const {
-        rowWin, 
-        colWin, 
-        leftDiagonalWin, 
-        rightDiagonalWin
-    } = winningCells
-    const cells = document.querySelectorAll(".cell")
-    cells.forEach(cell => {
-        const rowValue = cell.dataset.row
-        const colValue = cell.dataset.col
-        if (rowWin){
+function markWinningCells(winningCell, row, col){
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell =>{
+        const rowValue = Number(cell.dataset.row)
+        const colValue= Number(cell.dataset.col)
+        if (winningCell === 'rowWin'){
             if (rowValue === row){
                 cell.classList.add('winning')
             }
         }
-        else if (colWin){
+        if (winningCell === 'colWin'){
             if (colValue === col){
                 cell.classList.add('winning')
             }
         }
-        else if (leftDiagonalWin){
+        if (winningCell === 'leftDiagonalWin'){
             if (rowValue === colValue){
                 cell.classList.add('winning')
             }
-        } else if (rightDiagonalWin){
-            rightD.forEach(pair => {
-                const [r, c] = pair
-                
-                if (rowValue == r && colValue == c){
-                    cell.classList.add('winning')
-                }
-            })
+        }
+        if (winningCell === 'righDiagonalWin'){
+            if (rowValue + colValue === gridSize - 1){
+                cell.classList.add('winning')
+            }
         }
     })
-
 }
 
-function start(gridSize){
-    game = Array.from(Array(gridSize), () => Array(gridSize).fill(0))
-    createBoard(gridSize)
+function start(){
+    createBoard()
+    fillBoard()
+    currentStatus.textContent = `${currentPlayer}'s Move`
     addEventListener()
-    button.addEventListener('click', ()=>{
-        resetGame()
-    })
 }
-start(3)
 
+start()
